34332999bc2be4e8bc2de709d1843885fea7f645
Gustavo do Bem Ferreira <gustavodobemferreira@gmail.com>
2025-07-02 15:18:19 -0300

    Atualização feitas pelo Rafael (Tutor)

--- src/Grafo.cpp
+++ src/Grafo.cpp
@@ -169,8 +169,3 @@ vector<char> Grafo::periferia() {
     cout<<"Metodo nao implementado"<<endl;
     return {};
 }
-
-vector<char> Grafo::vertices_de_articulacao() {
-    cout<<"Metodo nao implementado"<<endl;
-    return {};
-}
